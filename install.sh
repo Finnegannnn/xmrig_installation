640f3b4786d06cc468cecca3c9731772f14fbbdd
Finnegannnn <thomsen-f@hotmail.com>
2026-03-20 14:04:28 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-sudo apt-get update && sudo apt-get upgrade -y
-sudo apt install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev 
+sudo apt-get install pv
+sudo apt-get update && sudo apt-get upgrade -y | pv
+sudo apt install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev | pv
 cd ~ 
 mkdir cryto_mining 
 cd cryto_mining
-git clone https://github.com/xmrig/xmrig.git
+git clone https://github.com/xmrig/xmrig.git | pv
 cd xmrig
 mkdir build
 cd build 
-cmake ..
-make -j$(nproc)
+cmake .. | pv
+make -j$(nproc) | pv
+echo "Build is complete and located in '~/crypto_mining/xmirg'"
