c0db7872da03460a19ebd4f5c0c0119c3d53da97
Finnegannnn <thomsen-f@hotmail.com>
2026-03-20 14:27:49 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 sudo apt-get install pv
+sudo apt-get intall bzip2
 sudo apt-get install torsocks | pv
 sudo apt-get update && sudo apt-get upgrade -y | pv
 sudo apt install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev | pv
