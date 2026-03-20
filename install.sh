51bd58d572faea317549fcf37c4c88217f04ae57
Finnegannnn <thomsen-f@hotmail.com>
2026-03-20 14:11:19 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 sudo apt-get install pv
+sudo apt-get install torsocks | pv
 sudo apt-get update && sudo apt-get upgrade -y | pv
 sudo apt install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev | pv
 cd ~ 
@@ -11,4 +12,4 @@ mkdir build
 cd build 
 cmake .. | pv
 make -j$(nproc) | pv
-echo "Build is complete and located in '~/crypto_mining/xmirg'"
+echo "Build is complete and located in '~/crypto_mining/xmirg/build/./xmrig'"
