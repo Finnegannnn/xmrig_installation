7477a4ec9515ab430456405fb10f92ff483ced6c
Finnegannnn <thomsen-f@hotmail.com>
2026-03-20 13:56:28 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+sudo apt-get update && sudo apt-get upgrade -y
+sudo apt install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev 
+cd ~ 
+mkdir cryto_mining 
+cd cryto_mining
+git clone https://github.com/xmrig/xmrig.git
+cd xmrig
+mkdir build
+cd build 
+cmake ..
+make -j$(nproc)
